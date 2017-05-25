9cd119f3383c65932b296302f9471a2d6062daef
ITS\cbotte1 <cbotte1@bidmc.harvard.edu>
2017-05-25 13:38:07 -0400

    Include write and other const flags

--- class/api.php
+++ class/api.php
@@ -96,7 +96,78 @@ class RCAPI{
     $this->curlHelper('user');
     return $this->curl_output;
   }
-
+//*******************************************************************************
+//*******************************************************************************
+//*******************************************************************************
+//  
+//  Sample Data Input:
+//      Type: json
+//      Def Overwrite Behavior: Normal
+//      Type: flat
+//      ReturnContent: ids
+//      ReturnFormat: json
+      
+//  [  
+//   {  
+//      "study_id":"1",
+//      "redcap_event_name":"event_1_arm_1",
+//      "date_enrolled":"2017-05-24",
+//      "patient_document":"",
+//      "first_name":"Chris",
+//      "last_name":"Botte",
+//      "address":"19 Elm St Burlington MA",
+//      "telephone_1":"(978) 433-0099",
+//      "email":"aaa@aol.com",
+//      "dob":"2011-05-18",
+//      "age":"6",
+//      "ethnicity":"1",
+//      "race":"0",
+//      "sex":"0",
+//      "given_birth":"1",
+//      "num_children":"3",
+//      "gym___0":"0",
+//      "gym___1":"0",
+//      "gym___2":"1",
+//      "gym___3":"1",
+//      "gym___4":"0",
+//      "aerobics___0":"0",
+//      "aerobics___1":"0",
+//      "aerobics___2":"0",
+//      "aerobics___3":"1",
+//      "aerobics___4":"0",
+//      "eat___0":"0",
+//      "eat___1":"1",
+//      "eat___2":"1",
+//      "eat___3":"0",
+//      "eat___4":"0",
+//      "drink___0":"1",
+//      "drink___1":"0",
+//      "drink___2":"0",
+//      "drink___3":"1",
+//      "drink___4":"1",
+//      "specify_mood":"82",
+//      "meds___1":"0",
+//      "meds___2":"0",
+//      "meds___3":"1",
+//      "meds___4":"1",
+//      "meds___5":"0",
+//      "height":"120",
+//      "weight":"100",
+//      "bmi":"69.4",
+//      "comments":"",
+//      "demographics_complete":"0"
+//   }
+//]
+  
+  public function writeData($data, $ovbehav = 'normal'){
+    $this->curlHelperWrite($data, $ovbehav);
+    return $this->curl_output;
+  }
+ 
+//*******************************************************************************
+//*******************************************************************************
+//*******************************************************************************
+  
   private function curlHelper($content, $fields=null, $forms=null){
 
     $data = array(
@@ -113,7 +184,7 @@ class RCAPI{
       'exportDataAccessGroups' => 'false'
     );
 
-    (DEVSTATUS === true) ? PC::debug($data, 'Curl Data Array For API: inside curlHelper') : "";
+    (DEVSTATUS === true && DEV_SHOW_DATA_ARRAY === true) ? PC::debug($data, 'Curl Data Array For API: inside curlHelper') : "";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->api_url);
@@ -130,13 +201,52 @@ class RCAPI{
     curl_close($ch);
     
     $this->curl_output = (ARRAYOUTPUT===true) ? json_decode($output, true) : $output;
-    (DEVSTATUS === true) ? PC::debug (json_decode($output, true), 'Curl Response For API (Converted to ARRAY for readability): inside curlHelper') : "";
-    (DEVSTATUS === true && SHOWORIGJSON === true) ? PC::debug ($output, 'Curl Response For API (Original): inside curlHelper') : "";
+    (DEVSTATUS === true && DEV_SHOW_API_RESPONSE === true) ? PC::debug (json_decode($output, true), 'Curl Response For API (Converted to ARRAY for readability): inside curlHelper') : "";
+    (DEVSTATUS === true && SHOW_ORIG_JSON === true && SHOW_API_RESPONSE === true) ? PC::debug ($output, 'Curl Response For API (Original): inside curlHelper') : "";
 
     if(strpos($output, 'error'))
-      throw new Exception("Err with API CURL:" . $output );
+      throw new Exception("Err with API CURL READ:" . $output );
   }
-}
+
+  private function curlHelperWrite($data, $ovbehav){
+    $data = array(
+    'token' => $this->token,
+    'content' => 'record',
+    'format' => 'json',
+    'type' => 'flat',
+    'overwriteBehavior' => $ovbehav,
+    'data' => $data,
+    'returnContent' => 'ids',
+    'returnFormat' => 'json'
+    );
+    
+    (DEVSTATUS === true && DEV_SHOW_DATA_ARRAY === true) ? PC::debug($data, 'Curl Data Array For API: inside curlHelper') : "";
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+    $output = curl_exec($ch);
+    curl_close($ch);  
+    
+    $this->curl_output = (ARRAYOUTPUT===true) ? json_decode($output, true) : $output;
+    (DEVSTATUS === true && DEV_SHOW_API_RESPONSE === true) ? PC::debug (json_decode($output, true), 'Curl Response For API (Converted to ARRAY for readability): inside curlHelper') : "";
+    (DEVSTATUS === true && SHOW_ORIG_JSON === true && SHOW_API_RESPONSE === true) ? PC::debug ($output, 'Curl Response For API (Original): inside curlHelper') : "";
+  
+    if(strpos($output, 'error') || $output=NULL)
+      throw new Exception("Err with API CURL WRITE:" . $output );
+    
+  }
+  
+  
+  }
 
 
 
--- testapi.php
+++ testapi.php
@@ -1,19 +1,27 @@
 <?php
+define("ARRAYOUTPUT", true);
 
 define("DEVSTATUS", true);
-define("ARRAYOUTPUT", true);
-define("SHOWORIGJSON", false);
+define("DEV_SHOW_ORIG_JSON", false);
+define("DEV_SHOW_DATA_ARRAY", TRUE);
+define("DEV_SHOW_API_RESPONSE", true);
+
 
 require_once 'class/api.php';
 
 try{
   
-  $a = new RCAPI('2C688C77C71DC34C8C5C46649B0A1FEB');
+  $a = new RCAPI('664337CD4431B8CC249CE44EF268233A');
   $a->getProjectInfo();
   $a->getFormMetadata(array('demographics'));
   $a->getFormData(array('demographics'));
 
-}
+  $data = '[{"study_id":"99","redcap_event_name":"event_1_arm_1","date_enrolled":"2017-05-24","patient_document":"","first_name":"CCChris","last_name":"Botte","address":"19 Elm St Burlington MA","telephone_1":"(978) 433-0099","email":"aaa@aol.com","dob":"2011-05-18","age":"6","ethnicity":"1","race":"0","sex":"0","given_birth":"1","num_children":"3","gym___0":"0","gym___1":"0","gym___2":"1","gym___3":"1","gym___4":"0","aerobics___0":"0","aerobics___1":"0","aerobics___2":"0","aerobics___3":"1","aerobics___4":"0","eat___0":"0","eat___1":"1","eat___2":"1","eat___3":"0","eat___4":"0","drink___0":"1","drink___1":"0","drink___2":"0","drink___3":"1","drink___4":"1","specify_mood":"82","meds___1":"0","meds___2":"0","meds___3":"1","meds___4":"1","meds___5":"0","height":"120","weight":"100","bmi":"69.4","comments":"","demographics_complete":"0"}]';
+  $data='[{"study_id":"99"}]';
+  $a->writeData($data, 'overwrite');
+  
+}  
+
 
 catch(Exception $e) {
   $msg = $e->getMessage();
